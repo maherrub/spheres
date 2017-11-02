5bade5486db5062900b9b82312deb76e32ee8a09
Ruban Maher <maher@live.com.sg>
2017-11-02 11:37:04 +0800

    allowed new host at settings

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'CHANGE_ME'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'spheresapp.mvetb6zdhf.ap-southeast-1.elasticbeanstalk.com',]
 
 
 # Application definition
